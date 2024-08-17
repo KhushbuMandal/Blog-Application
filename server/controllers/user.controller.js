4c173af185bfa77b0a2e97e2a7ae2c9d73a65c8d
KhushbuMandal <khushbumandal4459@gmail.com>
2024-08-17 23:33:24 +0530

    🚀 Completed with the User Login

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -66,8 +66,31 @@ _______________________________
 const login = async (req , res) => {
     try {
 
-        console.log (req.body)
-        res.status(200).json({message : req.body});
+        const {email , password} = req.body;
+
+        const userExist = await User.findOne({email});
+        console.log(userExist);
+
+        if (!userExist){
+            return res.status(400).json({message : "Invalid Credentials"});
+        }
+
+        const user = await userExist.comparePassword(password);
+
+        //res.status(200).json({ user : userExist});
+
+        if (user) {
+
+            res.status(200).json({
+                // message : userCreated , 
+                message : "Login Sucessful !!!",
+                token : await userExist.generateToken(), 
+                userId : userExist._id.toString()
+            });
+
+        }else {
+            res.status(401).json({message : "Inavlid email or password"});
+        }
         
         
     } catch (error) {
@@ -78,3 +101,4 @@ const login = async (req , res) => {
 
 
 module.exports = {home , register , login};
+

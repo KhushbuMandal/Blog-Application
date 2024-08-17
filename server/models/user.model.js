87e8ba24528e8eee3af205c5bea6250fa94601d9
KhushbuMandal <khushbumandal4459@gmail.com>
2024-08-17 18:22:36 +0530

    🚀Encrypted the password

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require ("mongoose");
+const bcrypt = require ("bcryptjs")
 
 
 const userSchema = new mongoose.Schema({
@@ -15,6 +16,36 @@ const userSchema = new mongoose.Schema({
         required: true,
     }
 });
+
+//secure the password with the bcrypt
+// database me save krene se phele run hoga
+userSchema.pre ("save", async function(next){
+    // console.log("pre method" , this);
+ 
+    const user = this;
+ 
+    if (!user.isModified("password")){
+     next();
+    }
+ 
+    //means first time password ban rha hai
+    try{
+ 
+     const saltRound = await bcrypt.genSalt(10);
+     const hash_password = await bcrypt.hash(user.password , saltRound);
+     user.password = hash_password;
+ 
+    }catch(error) {
+     next(error);
+    }
+ 
+ 
+ });
+
+//compare the password -> during Login
+userSchema.methods.comparePassword = async function (password) {
+    return  await bcrypt.compare(password , this.password);
+}
 
 
 const User = mongoose.model("User" , userSchema)

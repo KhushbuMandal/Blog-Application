30f9d48735e0c3fe06bfef282f22a248ca2c3eb3
KhushbuMandal <khushbumandal4459@gmail.com>
2024-08-18 00:46:01 +0530

    Integrated Zod Validation in login and registration

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,15 +1,21 @@
 const express = require ("express");
 const userControllers = require("../controllers/user.controller");
+const validate = require("../middlewares/validator.middleware");
+const { signupSchema, loginSchema } = require("../validators/user.validation");
 const router = express.Router();
 
 
 //Routes Declaration
 router.route("/").get(userControllers.home)
 
-router.route("/register").post(userControllers.register)
+router
+.route("/register")
+.post(validate(signupSchema) ,userControllers.register)
 
 
-router.route("/login").post(userControllers.login)
+router
+.route("/login")
+.post(validate(loginSchema), userControllers.login)
 
 
 module.exports = router;

1f74cf6bf083f5d95266c8558a330fe4e921f0e0
KhushbuMandal <khushbumandal4459@gmail.com>
2024-08-17 17:37:26 +0530

    Store the Registration data in database

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user.model");
+
 /*
 _______________________________
 
@@ -24,8 +26,22 @@ _______________________________
 
 const register = async (req , res) => {
     try {
-        console.log (req.body)
-        res.status(200).json({message : req.body});
+       
+        const {username , email ,  password} = req.body;
+
+        const userExist = await User.findOne({email : email});
+
+        if (userExist) {
+            return res.status(400).json({message : "email already exists"});
+        }
+
+        const userCreated = await  User.create ({
+            username ,
+            email ,
+            password
+        });
+
+        res.status(201).json({message : userCreated})
         
         
     } catch (error) {

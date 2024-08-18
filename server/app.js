502225ae3326463f6190f98f2db8c108f29b085b
KhushbuMandal <khushbumandal4459@gmail.com>
2024-08-18 14:43:16 +0530

    ✌️ Done with Contact Us

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require ("express");
 const app = express();
 const userRouter = require("./routes/user.routes")
+const contactRouter = require("./routes/contact.routes")
 
 app.use(express.json());
 
 app.use("/api/user", userRouter);
+app.use("/api/form" , contactRouter);
 
 module.exports = app;
